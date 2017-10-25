eb2b0186c597f48803d6908bfaaee1624d9853c8
Yuliya Shakhrai <32871561+YuliyaShakhrai@users.noreply.github.com>
2017-10-25 18:32:40 +0300

    Add files via upload

---
+++ EucludianGCDAlgorithms/GetGCD.cs
@@ -0,0 +1,224 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace GCDAlgorithms
+{
+    /// <summary>
+    ///Class Contains methods for getting the greatest common divisor of integer numbers and receiving execution time.
+    /// </summary>
+    public static class GetGCD
+    {
+        #region Public methods
+        /// <summary>
+        /// The method for getting the greatest common divisor of two integer numbers by Euclidean algorithm.
+        /// </summary>
+        /// <param name="first">First number</param>
+        /// <param name="second">Second number</param>
+        /// <returns>The greatest common divisor of two integer numbers</returns>
+        public static int GetGCDEuclidean (int first, int second)
+        {
+            #region Exceptions
+            if (first == 0 && second == 0) throw new ArgumentNullException("Two numbers are zero.");
+            #endregion
+            if (first == 0) return second;
+            if (second == 0) return first;
+
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+
+            while (first != second)
+            {
+                if (first > second) first -= second;
+                else second -= first;
+            }
+            return first;
+        }
+
+        /// <summary>
+        /// The method for getting the greatest common divisor of two integer numbers by Euclidean algorithm and receiving execution time.
+        /// </summary>
+        /// <param name="first">First number</param>
+        /// <param name="second">Second number</param>
+        /// <returns>The greatest common divisor of two integer numbers</returns>
+        public static int GetGCDEuclidean(out long milliseconds, int first, int second)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcd = GetGCDEuclidean(first, second);
+            stopwatch.Stop();
+            milliseconds = stopwatch.ElapsedMilliseconds;
+            return gcd;
+        }
+
+        /// <summary>
+        /// The method for getting the greatest common divisor of array of integers by Euclidean algorithm.
+        /// </summary>
+        /// <param name="intArray">Array of integer numbers</param>
+        /// <returns>The greatest common divisor of two integer numbers</returns>
+        public static int GetGCDEuclidean(params int[] intArray)
+        {
+            #region Exceptions
+            if (intArray.Length < 2)
+            {
+                throw new ArgumentException("Array contains less than 2 numbers.");
+            }
+            if (IsArrayOfZeros(intArray))
+            {
+                throw new ArgumentException("Array contains only zeros.");
+            }
+            #endregion
+
+            int[] tempArray = AdaptArray(intArray);
+            int result = GetGCDEuclidean(tempArray[0], tempArray[1]);
+            for (int i = 2; i < tempArray.Length; i++)
+            {
+                result = GetGCDEuclidean(result, tempArray[i]);
+            }
+            return result;
+        }
+
+        /// <summary>
+        /// The method for getting the greatest common divisor of array of integers by Euclidean algorithm and receiving execution time.
+        /// </summary>
+        /// <param name="intArray">Array of integer numbers</param>
+        /// <returns>The greatest common divisor of two integer numbers</returns>
+        public static int GetGCDEuclidean(out long milliseconds, params int[] intArray)
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+            int gcd = GetGCDEuclidean(intArray);
+            stopwatch.Stop();
+            milliseconds = stopwatch.ElapsedMilliseconds;
+            return gcd;
+        }
+
+        /// The method for getting the greatest common divisor of two integer numbers by Stein algorithm.
+        /// </summary>
+        /// <param name="first">First number</param>
+        /// <param name="second">Second number</param>
+        /// <returns>The greatest common divisor of two integer numbers</returns>
+        public static int GetGCDStein(int first, int second)
+        {
+            #region Exceptions
+            if (first == 0 && second == 0) throw new ArgumentNullException("Two numbers are zero.");
+            #endregion
+            if (first == 0) return second;
+            if (second == 0) return first;
+            if (first == 1 || second == 1) return 1;
+
+            first = Math.Abs(first);
+            second = Math.Abs(second);
+
+            if ((first % 2 == 0) && (second % 2 == 0))
+            {
+                return 2 * GetGCDStein(first / 2, second / 2);
+            }
+            if ((first % 2 == 0) && (second % 2 != 0))
+            {
+                return GetGCDStein(first / 2, second);
+            }
+            if ((first % 2 != 0) && (second % 2 == 0))
+            {
+                return GetGCDStein(first, second / 2);
+            }
+            return GetGCDStein(second, Math.Abs(first - second));
+        }
+
+        /// <summary>
+        /// The method for getting the greatest common divisor of array of integers by Stein algorithm.
+        /// </summary>
+        /// <param name="intArray">Array of integer numbers</param>
+        /// <returns>The greatest common divisor of two integer numbers</returns>
+        public static int GetGCDStein(params int[] intArray)
+        {
+            #region Exceptions
+            if (intArray.Length < 2)
+            {
+                throw new ArgumentException("Array contains less than 2 numbers.");
+            }
+            if (IsArrayOfZeros(intArray))
+            {
+                throw new ArgumentException("Array contains only zeros.");
+            }
+            #endregion
+
+            int[] tempArray = AdaptArray(intArray);
+            int result = GetGCDStein(tempArray[0], tempArray[1]);
+            for (int i = 2; i < tempArray.Length; i++)
+            {
+                result = GetGCDStein(result, tempArray[i]);
+            }
+            return result;
+        }
+
+        #endregion
+
+        #region Private methods
+        ///<summary>
+        /// The method for checking is array of zeros.
+        /// </summary>
+        /// <param name="intArray">Array of integer numbers</param>
+        /// <returns>Returns flag of checking</returns>
+        private static bool IsArrayOfZeros(int[] intArray)
+        {
+            bool flag = false;
+            for (int i = 0; i < intArray.Length; i++)
+            {
+                if (intArray[i] != 0)
+                {
+                    flag = false;
+                    break;
+                }
+                else
+                    flag = true;
+            }
+            return flag;
+        }
+
+        /// <summary>
+        /// The method for adapting array for gcd algorithms.
+        /// </summary>
+        /// <param name="intArray">Array of integer numbers</param>
+        /// <returns>The adapted array of integers</returns>
+        private static int[] AdaptArray(int[] intArray)
+        {
+            #region Counting size for new int[] without zeros
+            int size = 0;
+            int[] sortedArray = intArray.OrderBy(x => x == 0).ToArray();
+            for (int i = 0; i < sortedArray.Length; i++)
+            {
+                if (sortedArray[i] != 0)
+                    size++;
+            }
+            #endregion
+
+            #region Adapting array depending on the scenario
+            if (size == intArray.Length)
+            {
+                return intArray;
+            }
+            if (size == 1)
+            {
+                int[] adaptedArray = new int[size + 1];
+                adaptedArray[0] = sortedArray[0];
+                adaptedArray[1] = 0;
+                return adaptedArray;
+            }
+            else
+            {
+                int[] adaptedArray = new int[size];
+                for (int i = 0; i < size; i++)
+                {
+                    adaptedArray[i] = sortedArray[i];
+                }
+                return adaptedArray;
+            }
+            #endregion
+        }
+        #endregion
+    }
+}
---
+++ ExtensionMethods/ExtensionMethod.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExtensionMethods
+{
+    /// <summary>
+    ///Class Contains method for converting double-precision floating point value to string representation by binary format.
+    /// </summary>
+    public static class ExtensionMethod
+    {
+        #region Constants
+        private const int ExponentBits = 11;
+        private const int MantissaBits = 52;
+        #endregion
+
+        #region Public method
+        public static string DoubleToBinaryString(this double doubleNumber)
+        {
+            string result = string.Empty;
+            string fractionPart = string.Empty;
+
+            if (doubleNumber >= 0) result += "0";
+            else result += "1";
+
+            string[] splitted = doubleNumber.ToString().Split('.');
+            string integerPart = IntegerPartToBinary(Math.Abs(long.Parse(splitted[0])));
+            if (splitted.Length == 1)
+            {
+                fractionPart = FractionPartToBinary(0, MantissaBits - integerPart.Length + 1);
+            }
+            else
+            {
+                fractionPart = FractionPartToBinary(double.Parse("0." + splitted[1]), MantissaBits - integerPart.Length + 1);
+            }
+            string exponentPart = IntegerPartToBinary(integerPart.Length - 1 + 1023);
+
+            for (int i = 0; i < exponentPart.Length - ExponentBits; i++)
+            {
+                exponentPart += "0";
+            }
+
+            result += exponentPart + GetMantissa(integerPart, fractionPart);
+            return result;
+
+        }
+        #endregion
+
+        #region Private methods
+        private static string IntegerPartToBinary(long integerPart)
+        {
+            var integerPartString = new StringBuilder();
+
+            for (var i = sizeof(long) * 8 - 1; i >= 0; i--)
+            {
+                integerPartString.Append((integerPart & ((long)1 << i)) != 0 ? '1' : '0');
+            }
+
+            return integerPartString.ToString().TrimStart('0');
+        }
+
+        public static string FractionPartToBinary(double fraction, int maxLength)
+        {
+
+            var fractionPartString = new StringBuilder();
+
+            if (fraction == 0)
+            {
+                for (int i = 0; i < maxLength - 1; i++)
+                {
+                    fractionPartString.Append("0");
+                }
+                return fractionPartString.ToString();
+            }
+            for (int i = 0; i < maxLength; i++)
+            {
+                fraction *= 2;
+                if (fraction == 1)
+                {
+                    fractionPartString.Append("1");
+                    return fractionPartString.ToString();
+                }
+                if (fraction > 1)
+                {
+                    fractionPartString.Append("1");
+                    fraction--;
+                }
+                else
+                {
+                    fractionPartString.Append("0");
+                }
+                fraction = Math.Round(fraction, 10);
+            }
+
+            return fractionPartString.ToString();
+        }
+
+        private static string GetMantissa(string integerPart, string fractionPart)
+        {
+            var strBuild = new StringBuilder();
+            char[] ch = integerPart.ToCharArray();
+            for (int i = 1; i < ch.Length; i++)
+            {
+                strBuild.Append(ch[i]);
+            }
+            strBuild.Append(fractionPart);
+
+            for (var i = 0; i <= MantissaBits - integerPart.Length - fractionPart.Length; i++)
+            {
+                strBuild.Append("0");
+            }
+            strBuild.ToString();
+            int d = strBuild.Length;
+            return strBuild.ToString();
+        }
+        #endregion
+
+
+
+    }
+}
